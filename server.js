f4f3d7ea5c708e0ec71ef78871a5e7eb2093378d
Clark Weckmann <cweckmaniii@gmail.com>
2017-12-06 06:36:42 -0600

    Updates

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 var express = require('express');
 var app = express();
-app.use("/sites", express.static('sites'));
 var Pageres = require('pageres');
+app.use("/sites", express.static('sites'));
 app.get("/", function (req, res) {
   res.send('Okay');
 });
 app.get("/lense", function (req, res) {
-  var imgName = Math.floor(100000000 +
-    Math.random() * 900000000);
-  var imgLocation = "/sites/" +
-    imgName;
+  var imgName = Math.floor(100000000 + Math.random() * 900000000);
+  var imgLocation = "/sites/" + imgName;
   var redirectToPng = req.query.redirect;
   var imgUrl = req.query.url;
   var imgWidth = req.query.width;
@@ -32,15 +30,7 @@ app.get("/lense", function (req, res) {
       imgHeight
     ], {
       crop: true
-    }).dest('./sites/').run().then(() => console.log('done'));
-    if (redirectToPng === true) {
-      res.redirect(imgLocation);
-    } else {
-      res.send(JSON.stringify({
-        location: imgLocation +
-          '.png'
-      }));
-    }
+      }).dest('./sites/').run().then(() => redirection());
   }
   if (imgUrl && imgHeight && imgWidth && (!imgCrop || imgCrop == false)) {
     var pageres = new Pageres({
@@ -51,15 +41,7 @@ app.get("/lense", function (req, res) {
       imgHeight
     ], {
       crop: false
-    }).dest('./sites/').run().then(() => console.log('done'));
-    if (redirectToPng === true) {
-      res.redirect(imgLocation);
-    } else {
-      res.send(JSON.stringify({
-        location: imgLocation +
-          '.png'
-      }));
-    }
+    }).dest('./sites/').run().then(() => redirection());
   }
   if (imgUrl && !imgHeight && !imgWidth && (!imgCrop || imgCrop == false)) {
     var pageres = new Pageres({
@@ -67,15 +49,7 @@ app.get("/lense", function (req, res) {
       filename: imgName
     }).src(imgUrl, ["1080x720"], {
       crop: false
-    }).dest('./sites/').run().then(() => console.log('done'));
-    if (redirectToPng === true) {
-      res.redirect(imgLocation);
-    } else {
-      res.send(JSON.stringify({
-        location: imgLocation +
-          '.png'
-      }));
-    }
+      }).dest('./sites/').run().then(() => redirection());
   }
   if (imgUrl && !imgHeight && !imgWidth && (imgCrop || imgCrop == true)) {
     var pageres = new Pageres({
@@ -83,17 +57,23 @@ app.get("/lense", function (req, res) {
       filename: imgName
     }).src(imgUrl, ["1080x720"], {
       crop: true
-    }).dest('./sites/').run().then(() => console.log('done'));
-    if (redirectToPng === true) {
-      res.redirect(imgLocation);
-    } else {
-      res.send(JSON.stringify({
-        location: imgLocation +
-          '.png'
-      }));
-    }
+      }).dest('./sites/').run().then(() => redirection());
   }
+function redirection() {
+  if (redirectToPng == 'true') {
+    res.writeHead(200, {'Content-Type': 'image/png' });
+    res.sendFile(__dirname +
+        imgLocation +
+        ".png");
+  } else {
+    res.send(JSON.stringify({
+      location: imgLocation +
+          '.png'
+    }));
+  }
+}
 });
+
 
 var listener = app.listen(3000, function () {
   console.log('Your app is listening on port ' +
