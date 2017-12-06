e00ae157aa1fbfb74b7fe6902a989e87e78da30a
Clark Weckmann <cweckmaniii@gmail.com>
2017-12-06 05:49:33 -0600

    Formatting For Local

--- server.js
+++ server.js
@@ -1,62 +1,64 @@
 var express = require('express');
 var app = express();
 app.use("/sites", express.static('sites'));
-const Pageres = require('pageres');
+var Pageres = require('pageres');
 app.get("/", function (req, res) {
   res.send('Okay');
 });
 app.get("/lense", function (req, res) {
-  var imgName = Math.floor(100000000 + Math.random() * 900000000);
-  var imgLocation = "/sites/" + imgName;
+  var imgName = Math.floor(100000000 +
+    Math.random() * 900000000);
+  var imgLocation = "/sites/" +
+    imgName;
   var redirectToPng = req.query.redirect;
-  if (req.query.url) {
-    var imgUrl = req.query.url;
-  };
-  if (req.query.width) {
-    var imgWidth = req.query.width;
-  };
-  if (req.query.height) {
-    var imgHeight = req.query.height;
-  };
-  if (req.query.crop) {
-    var imgCrop = req.query.crop;
-  };
+  var imgUrl = req.query.url;
+  var imgWidth = req.query.width;
+  var imgHeight = req.query.height;
+  var imgCrop = req.query.crop;
   if (!req.query.url) {
     res.send('No Url Specified')
-  };
+  }
   if (imgHeight && !imgWidth || imgWidth && !imgHeight) {
     res.send(JSON.stringify({
       error: "width and height must both be specified"
     }))
-  };
+  }
   if (imgUrl && imgHeight && imgWidth && imgCrop == true) {
-    const pageres = new Pageres({
+    var pageres = new Pageres({
       delay: 2,
       filename: imgName
-    }).src(imgUrl, [imgWidth + 'x' + imgHeight], {
+    }).src(imgUrl, [imgWidth +
+      'x' +
+      imgHeight
+    ], {
       crop: true
-    }).dest('./sites/')
-    .run()
-    .then(() => console.log('done'));
-        if (redirectToPng === true){
+    }).dest('./sites/').run().then(() => console.log('done'));
+    if (redirectToPng === true) {
       res.redirect(imgLocation);
     } else {
-      res.send(JSON.stringify({location: imgLocation + '.png'}));
+      res.send(JSON.stringify({
+        location: imgLocation +
+          '.png'
+      }));
     }
   }
   if (imgUrl && imgHeight && imgWidth && (!imgCrop || imgCrop == false)) {
-    const pageres = new Pageres({
+    var pageres = new Pageres({
       delay: 2,
       filename: imgName
-    }).src(imgUrl, [imgWidth + 'x' + imgHeight], {
+    }).src(imgUrl, [imgWidth +
+      'x' +
+      imgHeight
+    ], {
       crop: false
-    }).dest('./sites/')
-    .run()
-    .then(() => console.log('done'));
-    if (redirectToPng === true){
+    }).dest('./sites/').run().then(() => console.log('done'));
+    if (redirectToPng === true) {
       res.redirect(imgLocation);
     } else {
-      res.send(JSON.stringify({location: imgLocation + '.png'}));
+      res.send(JSON.stringify({
+        location: imgLocation +
+          '.png'
+      }));
     }
   }
 });
